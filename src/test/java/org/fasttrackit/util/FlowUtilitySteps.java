a74b248f262ad625172f894e8daa8a5e665643fd
dcosman <cosmandaniel@gmail.com>
2015-02-19 15:52:34 +0200

    Initial commit

---
+++ src/test/java/org/fasttrackit/util/FlowUtilitySteps.java
@@ -0,0 +1,39 @@
+package org.fasttrackit.util;
+
+import com.sdl.selenium.web.WebDriverConfig;
+import com.sdl.selenium.web.utils.Utils;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.Dimension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FlowUtilitySteps extends TestBase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowUtilitySteps.class);
+
+    @Given("^I wait (\\d+) seconds$")
+    public void wait_seconds(int seconds) {
+        LOGGER.info("Waiting {} seconds", seconds);
+        Utils.sleep(seconds * 1000);
+    }
+
+    @When("^I open url \"([^\"]*)\"$")
+    public void I_open_LC_path(String url) throws Throwable {
+        AppUtils.openUrl(url);
+    }
+
+    @Given("^I set browser size to (\\d+), (\\d+)$")
+    public void setBrowserSize(int width, int height) throws Throwable {
+        LOGGER.info("I set browser size to ({}, {})", width, height);
+        WebDriverConfig.getDriver().manage().window().setSize(new Dimension(width, height));
+    }
+
+    @Given("^I maximize browser$")
+    public void maximizeBrowser() throws Throwable {
+        WebDriverConfig.getDriver().manage().window().maximize();
+    }
+
+
+}
---
+++ src/test/java/org/fasttrackit/util/TestyUtilitySteps.java
@@ -0,0 +1,75 @@
+package org.fasttrackit.util;
+
+import com.sdl.selenium.web.SearchType;
+import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.button.InputButton;
+import com.sdl.selenium.web.form.SimpleTextField;
+import com.sdl.selenium.web.link.WebLink;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestyUtilitySteps /*extends TestBase*/ {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestyUtilitySteps.class);
+
+    @When("^I click on link with text \"([^\"]*)\"$")
+    public void I_click_on_link_with_text(String text) {
+        WebLink link = new WebLink().setText(text);
+        link.assertClick();
+    }
+    
+    @When("^I click on input button with text \"([^\"]*)\"$")
+    public void I_click_on_input_button_with_text(String text) throws Throwable {
+        InputButton button = new InputButton().setText(text);
+        button.assertClick();
+    }
+
+    @Then("^I should see an element with text \"([^\"]*)\"$")
+    public void assertHaveElementWithText(String text) throws Throwable {
+        WebLocator element = new WebLocator().setText(text);
+        //assertThatElementIsReady(element);
+    }
+
+    @Then("^I should see following elements with texts \"(.*)\"$")
+    public void assertHaveElementsWithText(List<String> texts) throws Throwable {
+        for (String text : texts) {
+            assertHaveElementWithText(text);
+        }
+    }
+
+    @When("^I type \"([^\"]*)\" into \"([^\"]*)\" field$")
+    public void typeIntoField(String value, String label) throws Throwable {
+        SimpleTextField field = new SimpleTextField().setLabel(label, SearchType.DEEP_CHILD_NODE_OR_SELF);
+        field.setValue(value);
+    }
+
+    public static void main(String[] args) {
+        SimpleTextField field = new SimpleTextField().setLabel("Password", SearchType.DEEP_CHILD_NODE_OR_SELF);
+
+        WebLocator header = new WebLocator().setTag("header");
+
+        WebLink link = new WebLink().setContainer(header).setText("Log In");
+        WebLink cart = new WebLink().setContainer(header).setText("Cart");
+
+        LOGGER.debug(link.getPath());
+    }
+
+    @Then("^text field with label \"([^\"]*)\" should have value \"([^\"]*)\"$")
+    public void text_field_with_label_should_have_value(String label, String value) throws Throwable {
+        SimpleTextField field = new SimpleTextField().setLabel(label, SearchType.DEEP_CHILD_NODE_OR_SELF);
+        assertThat(field.getValue(), is(value));
+    }
+
+    @And("^I type \"([^\"]*)\" into field with id \"([^\"]*)\"$")
+    public void I_type_into_field_with_id(String value, String id) {
+        SimpleTextField field = new SimpleTextField().setId(id).setName("s");
+        field.setValue(value);
+    }
+}
