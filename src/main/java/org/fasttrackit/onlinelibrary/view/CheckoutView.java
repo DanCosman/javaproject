36db0d2e62671612ba97145ab3e528f00fcd42bd
dcosman <cosmandaniel@gmail.com>
2015-02-24 12:47:44 +0200

    refactor checkout steps improve checkout feature

--- src/main/java/org/fasttrackit/onlinelibrary/view/CheckoutView.java
+++ src/main/java/org/fasttrackit/onlinelibrary/view/CheckoutView.java
@@ -3,8 +3,6 @@ package org.fasttrackit.onlinelibrary.view;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.form.SimpleTextField;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,67 +14,42 @@ public class CheckoutView extends WebLocator {
     }
 
     public SimpleTextField firstNameField = new SimpleTextField(this).setId("billing_first_name");
-    public SimpleTextField lastName = new SimpleTextField(this).setId("billing_first_name");
-    public SimpleTextField address = new SimpleTextField(this).setId("billing_first_name");
+    public SimpleTextField lastNameField = new SimpleTextField(this).setId("billing_last_name");
+    public SimpleTextField addressField = new SimpleTextField(this).setId("billing_address_1");
+    public SimpleTextField cityField= new SimpleTextField(this).setId("billing_city");
+    public SimpleTextField postcodeField = new SimpleTextField(this).setId("billing_postcode");
+    public SimpleTextField emailField = new SimpleTextField(this).setId("billing_email");
+    public SimpleTextField phoneField = new SimpleTextField(this).setId("billing_phone");
+    public SimpleTextField accountPassword = new SimpleTextField(this).setId("account_password");
+    public SimpleTextField cardNumberField = new SimpleTextField(this).setId("stripe-card-number");
+    public SimpleTextField cardExpiryField = new SimpleTextField(this).setId("stripe-card-expiry");
+    public SimpleTextField cvcField = new SimpleTextField(this).setId("stripe-card-cvc");
+    public SimpleTextField termsCheckbox = new SimpleTextField(this).setId("terms");
 
-        @FindBy(id = "billing_last_name")
-        public WebElement lastNameField;
 
-        @FindBy(id = "billing_address_1")
-        public WebElement addressField;
-
-        @FindBy(id = "billing_city")
-        public WebElement cityField;
-
-        @FindBy(id = "billing_postcode")
-        public WebElement postcodeField;
-
-        @FindBy(id = "billing_email")
-        public WebElement emailField;
-
-        @FindBy(id = "billing_phone")
-        public WebElement phoneField;
-
-        @FindBy(id = "account_password")
-        public WebElement accountPassword;
-
-        @FindBy(id = "stripe-card-number")
-        public WebElement cardNumberField;
-
-        @FindBy(id = "stripe-card-expiry")
-        public WebElement cardExpiryField;
-
-        @FindBy(id = "stripe-card-cvc")
-        public WebElement cvcField;
-
-        @FindBy(id = "terms")
-        public WebElement termsCheckbox;
-
-        @FindBy(id = "place_order")
-        public WebElement orderButton;
-
-        public void populatePage() throws Throwable {
-            LOGGER.info("ajunge aici");
-            firstNameField.setValue(RandomStringUtils.randomAlphabetic(6));
-            lastNameField.sendKeys(RandomStringUtils.randomAlphabetic(6));
-            addressField.sendKeys(RandomStringUtils.randomAlphabetic(10));
-            cityField.sendKeys(RandomStringUtils.randomAlphabetic(10));
-            postcodeField.sendKeys("HP11 2TY");
-            emailField.sendKeys(RandomStringUtils.randomAlphabetic(5) + "@example.com");
-            phoneField.sendKeys(RandomStringUtils.randomNumeric(8));
-            accountPassword.sendKeys("password");
-            String cardNumber = "4111111111111111";
+    public void populatePage() throws Throwable {
+        LOGGER.info("ajunge aici");
+        firstNameField.setValue(RandomStringUtils.randomAlphabetic(6));
+        lastNameField.sendKeys(RandomStringUtils.randomAlphabetic(6));
+        addressField.sendKeys(RandomStringUtils.randomAlphabetic(10));
+        cityField.sendKeys(RandomStringUtils.randomAlphabetic(10));
+        postcodeField.sendKeys("HP11 2TY");
+        emailField.sendKeys(RandomStringUtils.randomAlphabetic(5) + "@example.com");
+        phoneField.sendKeys(RandomStringUtils.randomNumeric(8));
+        accountPassword.sendKeys("password");
+        String cardNumber = "4111111111111111";
+        cardNumberField.sendKeys(cardNumber);
+        if (!cardNumberField.getAttribute("value").equals(cardNumber))
             cardNumberField.sendKeys(cardNumber);
-            if (!cardNumberField.getAttribute("value").equals(cardNumber))
-                cardNumberField.sendKeys(cardNumber);
-            cardExpiryField.sendKeys("12/2018");
-            cvcField.sendKeys("123");
-        }
+        cardExpiryField.sendKeys("12/2018");
+        cvcField.sendKeys("123");
+        termsCheckbox.click();
+    }
 
-        public void placeOrder() {
-            if (!termsCheckbox.isSelected())
-                termsCheckbox.click();
-            orderButton.click();
-        }
+//        public void placeOrder() {
+//            if (!termsCheckbox.isSelected())
+//                termsCheckbox.click();
+//                orderButton.click();
+//            }
 
 }
